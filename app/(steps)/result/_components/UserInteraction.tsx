a21ca0bfbfb604712b78378e9e3666978795890c
moonki <moonki0623@naver.com>
2024-12-01 04:45:51 +0900

    WIP: 결과 확인 페이지 작업중

--- app/(steps)/result/_components/UserInteraction.tsx
+++ app/(steps)/result/_components/UserInteraction.tsx
@@ -4,13 +4,21 @@ const UserInteraction = () => {
   const handleAskEvent = () => {
     console.log("ask event");
   };
-  const handleShareEvent = () => {};
+  const handleShareEvent = () => {
+    console.log("share event");
+  };
   return (
     <div className="flex flex-col w-full gap-3 items-center my-3 mx-6">
-      <button className="w-[calc(100%-48px)] flex items-center justify-center bg-brown text-white text-sm tracking-[0.24px] leading-[150%] h-12">
+      <button
+        onClick={handleAskEvent}
+        className="w-[calc(100%-48px)] flex items-center justify-center bg-brown text-white text-sm tracking-[0.24px] leading-[150%] h-12"
+      >
         {`상대방`}에게도 부탁하기
       </button>
-      <button className="w-[calc(100%-48px)] flex items-center justify-center bg-pink text-white text-sm tracking-[0.24px] leading-[150%] h-12">
+      <button
+        onClick={handleShareEvent}
+        className="w-[calc(100%-48px)] flex items-center justify-center bg-pink text-white text-sm tracking-[0.24px] leading-[150%] h-12"
+      >
         우리 1년 SNS에 자랑하기
       </button>
     </div>
