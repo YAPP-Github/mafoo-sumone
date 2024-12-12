06a9c4f6b4e53f3b8d48935d06d2e97786061141
moonki <moonki0623@naver.com>
2024-12-13 01:54:54 +0900

    asset: Chevron asset 크기 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ const Header = ({ titleComponent, onClickPrev }: HeaderProps) => {
   return (
     <header className="relative flex h-14 w-full items-center justify-center px-4 py-3.5">
       <Chevron
-        width={24}
+        width={28}
         direction="left"
         onClick={onClickPrev ?? handleClickPrev}
         className="absolute left-4 focus:outline-none"
