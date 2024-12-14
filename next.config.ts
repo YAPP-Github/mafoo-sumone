fcd82a08ab273e9c025a856d311563102c4c3ed2
moonki <moonki0623@naver.com>
2024-12-14 16:48:53 +0900

    feat: 이미지 최적화

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "mafoo-sumone-event.imgix.net",
-      },
-    ],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [320, 420, 768, 1024, 1200, 1600],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "mafoo-sumone-event.imgix.net",
+    //   },
+    // ],
   },
   async redirects() {
     return [
