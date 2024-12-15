cbdcf08133d3da25e56191a6f8354b169be9ad2d
moonki <moonki0623@naver.com>
2024-12-15 23:18:50 +0900

    test: 기기별 width 대응

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: "auto",
+  width: "device-width",
   initialScale: 1,
   minimumScale: 1,
   maximumScale: 1,
